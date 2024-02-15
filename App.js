f8a174524bf5cb453fd0922d1c7829a9472d8870
ZiedKnani <zied.knani@medtech.tn>
2024-02-15 21:37:26 +0100

    Committing local changes before merge

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+=======
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LogIn from './screens/Frontend/LogIn';
+>>>>>>> 353f576 (Committing local changes before merge)
 
 import LogIn from "./screens/Frontend/LogIn";
 const App = () => {
   return (
     <View style={styles.container}>
+<<<<<<< HEAD
       <LogIn />
+=======
+     <LogIn/>
+>>>>>>> 353f576 (Committing local changes before merge)
       <StatusBar style="auto" />
     </View>
   );
